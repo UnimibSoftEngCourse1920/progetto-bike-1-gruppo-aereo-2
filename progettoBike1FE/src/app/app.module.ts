91eddc51e512fb0a6751f284f475e8f65f9f5b38
LorenzoNosotti1997 <l.nosotti1@campus.unimib.it>
2020-02-13 21:11:08 +0100

    add login

--- progettoBike1FE/src/app/app.module.ts
+++ progettoBike1FE/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GuardiaGuard } from './guardia.guard';
+import { LoginService } from './login.service';
+import { TokenService } from './token.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [LoginService, GuardiaGuard, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenService,
+    multi: true
+  }],  bootstrap: [AppComponent]
 })
 export class AppModule { }
