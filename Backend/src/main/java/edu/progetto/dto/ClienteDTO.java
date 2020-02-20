12ce617aaff1fae7fe10119824694ffe41329221
Oleg Stoianov <o.stoianov@campus.unimib.it>
2020-02-20 22:08:00 +0100

    Login with roles

--- Backend/src/main/java/edu/progetto/dto/ClienteDTO.java
+++ Backend/src/main/java/edu/progetto/dto/ClienteDTO.java
@@ -1,25 +1,35 @@
 package edu.progetto.dto;
 
+import edu.progetto.entity.Ruolo;
+
 public class ClienteDTO {
-	 private String name;
+	 private String nome;
 	 private String cognome;
 	 private String email;
 	 private String username;
 	 private String password;
+	 private Ruolo ruolo;
 	 
 
-	public ClienteDTO(String name, String cognome, String email, String username, String password) {
-		this.name = name;
+	public ClienteDTO(String nome, String cognome, String email, String username, String password, Ruolo ruolo) {
+		this.nome = nome;
 		this.cognome = cognome;
 		this.email = email;
 		this.username = username;
 		this.password = password;
+		this.ruolo = ruolo;
 	}
-	public String getName() {
-		return name;
+	public Ruolo getRuolo() {
+		return ruolo;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setRuolo(Ruolo ruolo) {
+		this.ruolo = ruolo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 	public String getCognome() {
 		return cognome;
--- Backend/src/main/java/edu/progetto/security/SecurityConfig.java
+++ Backend/src/main/java/edu/progetto/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package edu.progetto.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,14 +14,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import edu.progetto.service.CustomUserDetailsService;
+import edu.progetto.service.UserDetailsServiceImpl;
 
+
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		// securedEnabled = true,
+		// jsr250Enabled = true,
+		prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
 	@Autowired
-	private CustomUserDetailsService myUserDetailsService;
+	private UserDetailsServiceImpl myUserDetailsService;
 	
 	@Autowired
 	private RequestFilter jwtRequestFilter;
@@ -44,10 +52,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.cors().and().csrf().disable()
-				.authorizeRequests().antMatchers("/authenticate", "/register").permitAll().
-						anyRequest().authenticated().and().
-						exceptionHandling().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				.exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests()
+						.antMatchers("/auth/**").permitAll()
+						.antMatchers("/test/**").permitAll()
+//						.antMatchers("/admin/**").hasRole("ADMIN")
+//						.antMatchers("/student").hasRole("ROLE_STUDENT")
+//						.antMatchers("/generic").hasRole("ROLE_GENERIC")
+						.anyRequest().authenticated();		
+				
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 	}
